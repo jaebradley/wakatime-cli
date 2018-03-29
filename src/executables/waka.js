02ac20e3615b9fb836dae47a759c6444c6126d65
jaebradley <jae.b.bradley@gmail.com>
2018-03-28 23:03:04 -0700

    fix(setup): fix setup description

--- src/executables/waka.js
+++ src/executables/waka.js
@@ -6,6 +6,6 @@ import pkg from '../../package.json';
 
 program.version(pkg.version)
   .description('CLI that combines searching and installing npm packages')
-  .command('setup', 'Add API token')
+  .command('setup', 'Add API Key')
   .command('today', 'Get Daily Summary')
   .parse(process.argv);
