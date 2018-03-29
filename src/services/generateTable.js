6b23d16574133e2bc007d9670f6bdf5300cc9ad8
jaebradley <jae.b.bradley@gmail.com>
2018-03-28 23:04:01 -0700

    style(eslint): disable param reassign line

--- src/services/generateTable.js
+++ src/services/generateTable.js
@@ -2,7 +2,7 @@ import barHorizontal from 'bar-horizontal';
 
 const generateTable = (data) => {
   const inputs = data.reduce((obj, { name, percent }) => {
-    obj[name] = percent;
+    obj[name] = percent; // eslint-disable-line no-param-reassign
     return obj;
   }, {});
   barHorizontal(inputs, { labels: true });
