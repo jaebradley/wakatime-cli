d8f1e53f485c590a22753de66be67e3a1a67e746
jaebradley <jae.b.bradley@gmail.com>
2018-03-30 13:01:04 -0700

    feat(yesterday): add yesterday command

---
+++ src/executables/waka-yesterday.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { getDailySummary } from '../';
+
+const execute = async () => {
+  try {
+    // GROSS
+    const yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
+    await getDailySummary(yesterday);
+  } catch (error) {
+    console.error('😞  Rut ro, an error occurred');
+    console.error(error);
+  }
+};
+
+execute();
--- src/executables/waka.js
+++ src/executables/waka.js
@@ -8,4 +8,5 @@ program.version(pkg.version)
   .description('CLI that displays data from the WakaTime service')
   .command('setup', 'Add API Key')
   .command('today', 'Get Daily Summary')
+  .command('yesterday', 'Get Summary for Yesterday')
   .parse(process.argv);
